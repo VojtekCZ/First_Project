d38cf7c1cbbaf5031f544775f8e7724c9e266429
Vojtech Leskovsky <Leskivojt@gmail.com>
2022-10-15 19:41:49 +0200

    Java01

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import model.Product;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Product product = new Product( "Keyboard");
+        System.out.println(product.getName());
+        System.out.println("line");
+
+
+
+
+    }
+}
